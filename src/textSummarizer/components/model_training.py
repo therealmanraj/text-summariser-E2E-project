d7f1dbbbebeb6ba5c4f61eee1be4a4665d683e62
Manraj Singh <msingh209@mylangara.ca>
2024-12-20 20:13:31 -0800

    updated model training cuz my laptop cant handle the training

--- src/textSummarizer/components/model_training.py
+++ src/textSummarizer/components/model_training.py
@@ -15,12 +15,15 @@ class ModelTraining:
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
-        print(model_pegasus)
-        print(self.config.model_ckpt)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
         
+        # Load the dataset
         dataset_samsum_pt = load_from_disk(self.config.data_path)
-
+        
+        # Use only a small subset of the dataset for testing
+        small_train_dataset = dataset_samsum_pt["train"].shuffle(seed=42).select(range(5))  # First 100 samples
+        small_eval_dataset = dataset_samsum_pt["validation"].shuffle(seed=42).select(range(1))  # First 50 samples
+    
         trainer_args = TrainingArguments(
             output_dir=self.config.root_dir,
             num_train_epochs=self.config.num_train_epochs,
@@ -29,21 +32,23 @@ class ModelTraining:
             per_device_eval_batch_size=self.config.per_device_train_batch_size,
             weight_decay=self.config.weight_decay,
             logging_steps=self.config.logging_steps,
-            evaluation_strategy=self.config.evaluation_strategy,
+            eval_strategy=self.config.evaluation_strategy,
             eval_steps=self.config.eval_steps,
             save_steps=self.config.save_steps,
             gradient_accumulation_steps=self.config.gradient_accumulation_steps,
         )
-
-
-        trainer = Trainer(model=model_pegasus, args=trainer_args,
-                  tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                  train_dataset=dataset_samsum_pt["test"], 
-                  eval_dataset=dataset_samsum_pt["validation"])
+    
+        trainer = Trainer(
+            model=model_pegasus, 
+            args=trainer_args,
+            processing_class=tokenizer, 
+            data_collator=seq2seq_data_collator,
+            train_dataset=small_train_dataset, 
+            eval_dataset=small_eval_dataset
+        )
         
-        wandb.login(key="adaa721de476acf858912e6f7f868e52d8ea1fb1")
-
+        os.environ["WANDB_MODE"] = "disabled"
         trainer.train()
-
-        model_pegasus.save_pretrained(os.path.join(self.config.root_dir,"pegasus-samsum-model"))
-        tokenizer.save_pretrained(os.path.join(self.config.root_dir,"tokenizer"))
+    
+        model_pegasus.save_pretrained(os.path.join(self.config.root_dir, "pegasus-samsum-model"))
+        tokenizer.save_pretrained(os.path.join(self.config.root_dir, "tokenizer"))
